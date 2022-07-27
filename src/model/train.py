cad207d76f83c1a15198e88b8939e55384ea6559
mahalaxminanya <mahalaxminanya@gmail.com>
2022-07-27 16:57:09 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -22,7 +22,6 @@ def main(args):
 
     # train model
     model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-    return model
 
 
 def get_csvs_df(path):
@@ -42,7 +41,7 @@ def process_data(df):
 
     # train/test split
     X_train, X_test, y_train, y_test = \
-                                       train_test_split(X, y, test_size=0.30, random_state=0)
+    train_test_split(X, y, test_size=0.30, random_state=0)
 
     # return splits and encoder
     return X_train, X_test, y_train, y_test
