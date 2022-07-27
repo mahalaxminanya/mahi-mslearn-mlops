7f9a04d543f1c612a245d388c55937764cc62ef9
mahalaxminanya <mahalaxminanya@gmail.com>
2022-07-27 14:30:18 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -41,7 +41,7 @@ def process_data(df):
                'Age']].values, df['Diabetic'].values
 
     # train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, /
+    X_train, X_test, y_train, y_test = train_test_split(X, \
                                        y, test_size=0.30, random_state=0)
 
     # return splits and encoder
